008ece3765d2695267b3e5de0a1c4856432f9432
piluqui <piluqui@gmail.com>
2017-11-09 13:53:15 -0300

    Ejercicios

---
+++ futbol.pl
@@ -0,0 +1,9 @@
+semifinal1(river,lanus).
+semifinal2(gremio,barcelona).
+
+ganador(semifinal,lanus).
+ganador(semifinal,gremio).
+
+elimino(lanus,semi,river).
+elimino(lanus,cuartos,sanlorenzo).
+elimino(lanus,octavos,strongest).
